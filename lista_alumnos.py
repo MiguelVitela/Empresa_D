79c7dd79b52a0bbf36a38b01971acf058e428a8d
MiguelVitela <71278199+MiguelVitela@users.noreply.github.com>
2021-09-17 20:44:12 -0500

    se inscribieron 2 alumnos más

--- lista_alumnos.py
+++ lista_alumnos.py
@@ -1,3 +1,3 @@
-alumnos_reprobados = ['Alan', 'Rodrigo', 'Gerardo', 'Esteban', 'Alonso', 'Mario']
+alumnos_reprobados = ['Alan', 'Rodrigo', 'Gerardo', 'Esteban', 'Alonso', 'Mario', 'Francisco', 'Alfonso']
 print(alumnos_reprobados)
 print(len(alumnos_reprobados))
